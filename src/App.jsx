f46de013b79c82626806dcf60e9de1170604aac1
Jeff Grissom <jgrissom91@gmail.com>
2024-09-10 14:44:08 -0500

    [1.04] Create functional App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [countries, setCountries] = useState([]);
+
+  // this is the functional equivalent to componentDidMount
+  useEffect(() => {
+    // initial data loaded here
+    let fetchedCountries = [
+      { id: 1, name: 'United States', gold: 2, silver: 2, bronze: 3 },
+      { id: 2, name: 'China', gold: 3, silver: 1, bronze: 0 },
+      { id: 3, name: 'Germany', gold: 0, silver: 2, bronze: 2 },
+      { id: 4, name: 'France', gold: 2, silver: 2, bronze: 1 },
+      { id: 5, name: 'Spain', gold: 1, silver: 1, bronze: 0 },
+      { id: 6, name: 'United Kingdom', gold: 0, silver: 2, bronze: 3 },
+      { id: 7, name: 'Brazil', gold: 3, silver: 0, bronze: 0 },
+      { id: 8, name: 'Italy', gold: 2, silver: 2, bronze: 2 },
+      { id: 9, name: 'Switzerland', gold: 1, silver: 1, bronze: 2 },
+      { id: 10, name: 'Poland', gold: 0, silver: 2, bronze: 1 },
+      { id: 11, name: 'Sweden', gold: 0, silver: 3, bronze: 1 },
+      { id: 12, name: 'Ireland', gold: 2, silver: 1, bronze: 0 },
+      { id: 13, name: 'Scotland', gold: 3, silver: 0, bronze: 2 },
+    ]
+    setCountries(fetchedCountries);
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Olympic Medals</h1>
+      <ul>
+        {
+          countries.map(country => <li key={country.id}>{country.name}</li>)
+        }
+      </ul>
     </>
   )
 }
